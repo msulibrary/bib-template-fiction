2dde914343100dd51faa57b18c4f099aacc47804
jasonclark <jasclark@gmail.com>
2016-12-24 00:19:26 -0700

    set convention for concurrent variables

--- meta/scripts/main.js
+++ meta/scripts/main.js
@@ -5,25 +5,26 @@
   if (!win.addEventListener) {
     return;
   }
-  var linkclass = 'control',
-    activeclass = 'active',
-    enhanceclass = 'enhanced',
-    toggleClassName = function (element, toggleClass) {
-      var reg = new RegExp('(\\s|^)' + toggleClass + '(\\s|$)');
-      if (!element.className.match(reg)) {
-        element.className += ' ' + toggleClass;
-      } else {
-        element.className = element.className.replace(reg, '');
-      }
-    },
-    navListener = function (ev) {
-      ev = ev || win.event;
-      var target = ev.target || ev.srcElement;
-      if (target.className.indexOf(linkclass) !== -1) {
-        ev.preventDefault();
-        toggleClassName(doc.body, activeclass);
-      }
-    };
+  var 
+  linkclass = 'control',
+  activeclass = 'active',
+  enhanceclass = 'enhanced',
+  toggleClassName = function (element, toggleClass) {
+    var reg = new RegExp('(\\s|^)' + toggleClass + '(\\s|$)');
+    if (!element.className.match(reg)) {
+      element.className += ' ' + toggleClass;
+    } else {
+      element.className = element.className.replace(reg, '');
+    }
+  },
+  navListener = function (ev) {
+    ev = ev || win.event;
+    var target = ev.target || ev.srcElement;
+    if (target.className.indexOf(linkclass) !== -1) {
+      ev.preventDefault();
+      toggleClassName(doc.body, activeclass);
+    }
+  };
   doc.documentElement.className += ' ' + enhanceclass;
   doc.addEventListener('click', navListener, false);
 }(this, this.document));
@@ -37,7 +38,6 @@ function getElementsByText(scope, text) {
       list = list.concat(getElementsByText(element, text));
     }
   }
-
   // return scope (no match)
   return list.length ? list : scope;
 }
@@ -51,12 +51,13 @@ function getElementsByText(scope, text) {
   function updateURL() {
     var selection = win.getSelection();
     if (selection) {
-      var selectedText = selection.toString(),
-          selectedNode = selection.anchorNode && selection.anchorNode.parentElement;
+      var 
+      selectedText = selection.toString(),
+      selectedNode = selection.anchorNode && selection.anchorNode.parentElement;
       if (selectedText.length > 1) {
         var hash = '#' + encodeURIComponent(selectedText);
-
-        var elements = getElementsByText(document, selectedText),
+        var 
+        elements = getElementsByText(document, selectedText),
         length = elements.length,
         which = length && elements.indexOf(selectedNode);
 
